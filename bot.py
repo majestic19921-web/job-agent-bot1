bc3ef647c56a17e5cc93c40e5ffad252f9817744
majestic19921-web <majestic19921@gmail.com>
2026-06-08 13:24:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import requests
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+    requests.post(url, data=payload)
+
+if __name__ == "__main__":
+    send_message("✅ Job Agent Bot is now running successfully!")
